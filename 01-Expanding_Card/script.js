75d81c39c1a097f4068a2bfaa574f558292bff85
Wilson Andres Bonilla <wilsonbonilla2712@gmail.com>
2025-04-28 08:42:23 -0500

    Add proyecto tarjetas expndibles

---
+++ 01-Expanding_Card/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel")
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+
+    })
+})
+
+
+const removeActiveClass = () => {
+    panel.forEach(panel => {
+        panel.classlist.remove('active')
+    })
+}
+
